1e6aef1801f7509dfaeec2c131e10c5fa0fcad2f
Anjali <anjaliap1227@gmail.com>
2024-05-21 16:42:47 +0530

    file add

---
+++ src/FindMaxInt.java
@@ -0,0 +1,18 @@
+public class FindMaxInt {
+    public static void max(int a,int b,int c){
+        System.out.println("The Numbers are: "+a+","+b+","+c);
+        if(a > b && a > c){
+            System.out.println("Maximum number is: "+ a);
+        }else if (b > a && b > c) {
+            System.out.println("Maximum number is: "+ b);
+        }else {
+            System.out.println("Maximum number is: "+ c);
+        }
+    }
+    public static void main(String[] args) {
+        int a=5;
+        int b=8;
+        int c=9;
+        max(a,b,c);
+    }
+}
