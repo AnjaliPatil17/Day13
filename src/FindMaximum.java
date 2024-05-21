8808c8f17f9437d39212665a0239490b176cec5d
Anjali <anjaliap1227@gmail.com>
2024-05-21 16:48:10 +0530

    UC1-Find_maximum_Integer fille added

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,6 +1,6 @@
-public class FindMaxInt {
+public class FindMaximum {
     public static void max(int a,int b,int c){
-        System.out.println("The Numbers are: "+a+","+b+","+c);
+        System.out.println("The Integer Numbers are: "+a+","+b+","+c);
         if(a > b && a > c){
             System.out.println("Maximum number is: "+ a);
         }else if (b > a && b > c) {
