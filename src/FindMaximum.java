faeb537200fcc1e27ec094eade1e60d7a7ec6b1c
Anjali <anjaliap1227@gmail.com>
2024-05-24 17:05:20 +0530

    UC4-Find_maximum_using_sorting file added

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,43 +1,30 @@
+import java.util.Arrays;
 
-    public class FindMaximum<T extends Comparable<T>> {
-        private T var1;
-        private T var2;
-        private T var3;
+public class FindMaximum<T extends Comparable<T>> {
+    private T[] elements;
 
-        // Parameterized constructor
-        public FindMaximum(T var1, T var2, T var3) {
-            this.var1 = var1;
-            this.var2 = var2;
-            this.var3 = var3;
-        }
-
-        // Method to find the maximum among the three variables
-        public T testMaximum() {
-            return FindMaximum.testMaximum(var1, var2, var3);
-        }
-
-        // Static method to find the maximum among three values
-        public static <T extends Comparable<T>> T testMaximum(T var1, T var2, T var3) {
-            T max = var1;
-            if (var2.compareTo(max) > 0) {
-                max = var2;
-            }
-            if (var3.compareTo(max) > 0) {
-                max = var3;
-            }
-            return max;
-        }
-
-        // New test case to use the Generic Clas
-        public static void main(String[] args) {
-
-            FindMaximum<Integer> intMaxFinder = new FindMaximum<>(3, 5, 1);
-            System.out.println("Maximum Integer: " + intMaxFinder.testMaximum());
-
-            FindMaximum<Float> floatMaxFinder = new FindMaximum<>(6.6f, 8.8f, 7.7f);
-            System.out.println("Maximum Float: " + floatMaxFinder.testMaximum());
-
-            FindMaximum<String> stringMaxFinder = new FindMaximum<>("apple", "banana", "cherry");
-            System.out.println("Maximum String: " + stringMaxFinder.testMaximum());
-        }
+    public FindMaximum(T... elements) {
+        this.elements = elements;
     }
+
+    public T testMaximum() {
+        return FindMaximum.testMaximum(elements);
+    }
+
+    public static <T extends Comparable<T>> T testMaximum(T... elements) {
+        Arrays.sort(elements);
+        return elements[elements.length - 1];
+    }
+
+    public static void main(String[] args) {
+
+        FindMaximum<Integer> intMaxFinder = new FindMaximum<>(3, 5, 1, 10, 7);
+        System.out.println("Maximum Integer: " + intMaxFinder.testMaximum());
+
+        FindMaximum<Float> floatMaxFinder = new FindMaximum<>(6.6f, 8.8f, 7.7f, 9.9f, 5.5f,6.7f);
+        System.out.println("Maximum Float: " + floatMaxFinder.testMaximum());
+
+        FindMaximum<String> stringMaxFinder = new FindMaximum<>("apple", "banana", "cherry", "date", "elderberry");
+        System.out.println("Maximum String: " + stringMaxFinder.testMaximum());
+    }
+}
